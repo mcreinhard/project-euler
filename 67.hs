8f36988eae145e9fa7b4d1c87872f6754d0bef0f
Michael Reinhard <mcreinhard@gmail.com>
2014-12-23 20:27:56 -0800

    Clean up 67

--- 67.hs
+++ 67.hs
@@ -2,14 +2,14 @@
 import Euler
 import Data.MemoTrie
 
-maxPathSum :: [[Int]] -> Int -> Int -> Int
-maxPathSum = memo3 maxPathSum' where
-maxPathSum' tri i j
+maxPathSum :: [[Int]] -> (Int,Int) -> Int
+maxPathSum = memo2 maxPathSum' where
+  maxPathSum' tri (i,j)
     | i >= length tri = 0
-    | otherwise       = (tri!!i)!!j
-          + max (maxPathSum tri(i+1) j) (maxPathSum tri(i+1) (j+1))
+    | otherwise = (tri !! i) !! j
+                  + max (maxPathSum tri (i+1,j)) (maxPathSum tri (i+1,j+1))
 
 answer :: String -> Int
-answer = (\tri -> maxPathSum tri 0 0) . map (map readDecimal . words) . lines
+answer = (\tri -> maxPathSum tri (0,0)) . map (map readDecimal . words) . lines
 
 main = readFile "resources/p067_triangle.txt" >>= print . answer
